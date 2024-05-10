4fc8dc340a6028da5c5749a9f2f1472575b3bcac
eminorhan <aeminorhan@gmail.com>
2024-05-10 18:19:49 -0400

    add code (first pass)

---
+++ util/video_vit.py
@@ -0,0 +1,190 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+from functools import partial
+from itertools import repeat
+import collections.abc
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+def _ntuple(n):
+    def parse(x):
+        if isinstance(x, collections.abc.Iterable) and not isinstance(x, str):
+            return x
+        return tuple(repeat(x, n))
+    return parse
+
+to_2tuple = _ntuple(2)
+
+def drop_path(x, drop_prob = 0., training = False, scale_by_keep = True):
+    """Drop paths (Stochastic Depth) per sample (when applied in main path of residual blocks).
+
+    This is the same as the DropConnect impl I created for EfficientNet, etc networks, however,
+    the original name is misleading as 'Drop Connect' is a different form of dropout in a separate paper...
+    See discussion: https://github.com/tensorflow/tpu/issues/494#issuecomment-532968956 ... I've opted for
+    changing the layer and argument names to 'drop path' rather than mix DropConnect as a layer name and use
+    'survival rate' as the argument.
+    """
+    if drop_prob == 0. or not training:
+        return x
+    keep_prob = 1 - drop_prob
+    shape = (x.shape[0],) + (1,) * (x.ndim - 1)  # work with diff dim tensors, not just 2D ConvNets
+    random_tensor = x.new_empty(shape).bernoulli_(keep_prob)
+    if keep_prob > 0.0 and scale_by_keep:
+        random_tensor.div_(keep_prob)
+    return x * random_tensor
+
+
+class PatchEmbed(nn.Module):
+    """Image to Patch Embedding"""
+    def __init__(
+        self,
+        img_size=224,
+        patch_size=16,
+        in_chans=3,
+        embed_dim=768,
+        # temporal related:
+        frames=32,
+        t_patch_size=4,
+    ):
+        super().__init__()
+        img_size = to_2tuple(img_size)
+        patch_size = to_2tuple(patch_size)
+        assert img_size[1] % patch_size[1] == 0
+        assert img_size[0] % patch_size[0] == 0
+        assert frames % t_patch_size == 0
+        num_patches = ((img_size[1] // patch_size[1]) * (img_size[0] // patch_size[0]) * (frames // t_patch_size))
+        self.input_size = (frames // t_patch_size, img_size[0] // patch_size[0], img_size[1] // patch_size[1])
+        
+        print(f"img_size {img_size} patch_size {patch_size} frames {frames} t_patch_size {t_patch_size}")
+        
+        self.img_size = img_size
+        self.patch_size = patch_size
+
+        self.frames = frames
+        self.t_patch_size = t_patch_size
+        self.num_patches = num_patches
+
+        self.grid_size = img_size[0] // patch_size[0]
+        self.t_grid_size = frames // t_patch_size
+
+        kernel_size = [t_patch_size] + list(patch_size)
+        self.proj = nn.Conv3d(in_chans, embed_dim, kernel_size=kernel_size, stride=kernel_size)
+
+    def forward(self, x):
+        B, C, T, H, W = x.shape
+        assert (H == self.img_size[0] and W == self.img_size[1]), f"Input image size ({H}*{W}) doesn't match model ({self.img_size[0]}*{self.img_size[1]})."
+        assert T == self.frames
+        x = self.proj(x).flatten(3)
+        x = torch.einsum("ncts->ntsc", x)  # [N, T, H*W, C]
+        return x
+
+
+class Attention(nn.Module):
+    def __init__(self, dim, num_heads=8, qkv_bias=False, qk_scale=None, attn_drop=0.0, proj_drop=0.0, input_size=(4, 14, 14)):
+        super().__init__()
+        assert dim % num_heads == 0, "dim should be divisible by num_heads"
+        self.num_heads = num_heads
+        head_dim = dim // num_heads
+        self.scale = qk_scale or head_dim**-0.5
+
+        self.q = nn.Linear(dim, dim, bias=qkv_bias)
+        self.k = nn.Linear(dim, dim, bias=qkv_bias)
+        self.v = nn.Linear(dim, dim, bias=qkv_bias)
+        assert attn_drop == 0.0  # do not use
+        self.proj = nn.Linear(dim, dim)
+        self.proj_drop = nn.Dropout(proj_drop)
+        self.input_size = input_size
+        assert input_size[1] == input_size[2]
+
+    def forward(self, x):
+        B, N, C = x.shape
+        q = (self.q(x).reshape(B, N, self.num_heads, C // self.num_heads).permute(0, 2, 1, 3))
+        k = (self.k(x).reshape(B, N, self.num_heads, C // self.num_heads).permute(0, 2, 1, 3))
+        v = (self.v(x).reshape(B, N, self.num_heads, C // self.num_heads).permute(0, 2, 1, 3))
+
+        x = F.scaled_dot_product_attention(q, k, v)  # flash attention-2
+
+        x = x.transpose(1, 2).contiguous().view(B, N, C)
+        x = self.proj(x)
+        x = self.proj_drop(x)
+        return x
+
+
+class DropPath(nn.Module):
+    """Drop paths (Stochastic Depth) per sample  (when applied in main path of residual blocks)."""
+    def __init__(self, drop_prob = 0., scale_by_keep = True):
+        
+        super(DropPath, self).__init__()
+
+        self.drop_prob = drop_prob
+        self.scale_by_keep = scale_by_keep
+
+    def forward(self, x):
+        return drop_path(x, self.drop_prob, self.training, self.scale_by_keep)
+
+    def extra_repr(self):
+        return f"drop_prob={round(self.drop_prob,3):0.3f}"
+
+
+class Mlp(nn.Module):
+    """ MLP as used in Vision Transformer, MLP-Mixer and related networks"""
+    def __init__(self, in_features, hidden_features=None, out_features=None, act_layer=nn.GELU, norm_layer=None, bias=True, drop=0., use_conv=False):
+
+        super().__init__()
+
+        out_features = out_features or in_features
+        hidden_features = hidden_features or in_features
+        bias = to_2tuple(bias)
+        drop_probs = to_2tuple(drop)
+        linear_layer = partial(nn.Conv2d, kernel_size=1) if use_conv else nn.Linear
+
+        self.fc1 = linear_layer(in_features, hidden_features, bias=bias[0])
+        self.act = act_layer()
+        self.drop1 = nn.Dropout(drop_probs[0])
+        self.norm = norm_layer(hidden_features) if norm_layer is not None else nn.Identity()
+        self.fc2 = linear_layer(hidden_features, out_features, bias=bias[1])
+        self.drop2 = nn.Dropout(drop_probs[1])
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.act(x)
+        x = self.drop1(x)
+        x = self.norm(x)
+        x = self.fc2(x)
+        x = self.drop2(x)
+        return x
+
+
+class Block(nn.Module):
+    """
+    Transformer Block with specified Attention function
+    """
+    def __init__(
+        self, 
+        dim, 
+        num_heads,
+        mlp_ratio=4.0,
+        qkv_bias=False,
+        qk_scale=None,
+        drop=0.0,
+        attn_drop=0.0,
+        drop_path=0.0,
+        act_layer=nn.GELU,
+        norm_layer=nn.LayerNorm,
+        attn_func=Attention,
+    ):
+        super().__init__()
+        self.norm1 = norm_layer(dim)
+        self.attn = attn_func(dim, num_heads=num_heads, qkv_bias=qkv_bias, qk_scale=qk_scale, attn_drop=attn_drop, proj_drop=drop)
+        self.drop_path = DropPath(drop_path) if drop_path > 0.0 else nn.Identity()
+        self.norm2 = norm_layer(dim)
+        mlp_hidden_dim = int(dim * mlp_ratio)
+        self.mlp = Mlp(in_features=dim, hidden_features=mlp_hidden_dim, act_layer=act_layer, drop=drop)
+
+    def forward(self, x):
+        x = x + self.drop_path(self.attn(self.norm1(x)))
+        x = x + self.drop_path(self.mlp(self.norm2(x)))
+        return x
---
+++ utils.py
@@ -0,0 +1,64 @@
+"""
+Util functions
+"""
+import torch
+import models_mae, models_vit
+from huggingface_hub import hf_hub_download
+
+def get_available_models():
+    available_models = [
+        'mae_hvm1_none',
+        'vit_hvm1_none',
+        'vit_hvm1_ssv2-50shot',
+        'vit_hvm1_ssv2-10shot',
+        'vit_hvm1_kinetics-50shot',
+        'vit_hvm1_kinetics-10shot',
+        'vit_hvm1_imagenet-2pt',
+        'mae_hvm1@448_none',
+        'vit_hvm1@448_none',
+        'vit_hvm1@448_ssv2-50shot',
+        'vit_hvm1@448_ssv2-10shot',
+        'vit_hvm1@448_kinetics-50shot',
+        'vit_hvm1@448_kinetics-10shot',
+        'vit_hvm1@448_imagenet-2pt',
+        ]
+
+    return available_models
+
+def load_model(model_name):
+
+    # make sure model is valid
+    assert model_name in get_available_models(), 'Unrecognized model!'
+
+    # parse identifier
+    model_type, pretrain_data, finetune_data = model_name.split('_')
+
+    # checks
+    assert model_type in ['mae', 'vit'], 'Unrecognized model type!'
+    assert pretrain_data in ['hvm1', 'hvm1@448'], 'Unrecognized pretraining data!'
+    assert finetune_data in ['none', 'ssv2-50shot', 'kinetics-50shot', 'ssv2-10shot', 'kinetics-10shot', 'imagenet-2pt'], 'Unrecognized finetuning data!'
+
+    # download checkpoint from hf
+    ckpt_filename = pretrain_data + '_' + finetune_data + '.pth'
+    ckpt = hf_hub_download(repo_id='eminorhan/hvm-1', filename=ckpt_filename)
+
+    if model_type.startswith('mae'):
+        model = models_mae.mae_vit_huge_patch14()
+        ckpt = torch.load(ckpt, map_location='cpu')
+        msg = model.load_state_dict(ckpt['model'], strict=True)
+        print(f'Loaded with message: {msg}')
+    elif model_type.startswith('vit'):
+        if finetune_data.startswith('ssv2'):
+            num_classes = 174
+        elif finetune_data.startswith('kinetics'):
+            num_classes = 700
+        elif finetune_data.startswith('imagenet'):
+            num_classes = 1000
+        else:
+            num_classes = None
+        model = models_vit.vit_huge_patch14(num_classes=num_classes)
+        ckpt = torch.load(ckpt, map_location='cpu')['model']
+        msg = model.load_state_dict(ckpt, strict=False)
+        print(f'Loaded with message: {msg}')
+
+    return model
